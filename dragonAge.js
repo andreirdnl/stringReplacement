cd0160bfd69de7b64db01941f3ce237e03d15e01
andreirdnl <59996433+andreirdnl@users.noreply.github.com>
2020-08-19 16:54:25 +0300

    Add files via upload

---
+++ dragonAge.js
@@ -0,0 +1,17 @@
+var string =
+    "The Inquisitor must meet Varric on top of Skyhold's battlements to be introduced.";
+
+  var patches = [
+    // [start, end, patch]
+    [5, 14, "Conquistador"],
+    [26, 31, "King"],
+    [43, 49, "Palace"],
+  ];
+
+  let finalString = string;
+  for (let i = 0; i < patches.length; i++) {
+    let findWord = string.slice(patches[i][0] - 1, patches[i][1]);
+    console.log(findWord);
+    finalString = finalString.replace(findWord, patches[i][2]);
+  }
+  console.log(finalString);
